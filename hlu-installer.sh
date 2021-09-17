a9d9eda6a338504c10bb69a002088caacba5e783
sergeyhist <sergeyhist@yahoo.com>
2021-09-18 02:26:23 +0300

    Global + fex fixes

--- hlu-installer.sh
+++ hlu-installer.sh
@@ -2,16 +2,16 @@
 Bin_folder="/usr/local/bin"
 Hist_folder="$HOME/Hist"
 Exec_folder="$(dirname "$0")/source"
-if [[ ! -d "$Hist_folder/Configs" ]]; then mkdir -p "$Hist_folder/Configs"; fi
-noc=$(tput sgr0); rec=$(tput setaf 1); grc=$(tput setaf 2); blc=$(tput setaf 4); cyc=$(tput setaf 6); mac=$(tput setaf 5)
+if [[ ! -d "$Hist_folder" ]]; then mkdir -p "$Hist_folder"; fi
+noc=$(tput sgr0); rec=$(tput setaf 1); grc=$(tput setaf 2); cyc=$(tput setaf 6)
 if [[ -f "$Bin_folder/histgamehelper" ]]; then
 	echo -e "\n ${cyc}1${noc} - ${grc}Update${noc}"
 	echo -e " ${cyc}2${noc} - ${grc}Uninstall${noc}"
-	echo -e "\nChoose ${cyc}option${noc} from the ${grc}list${noc} above:"; read -e "installer_ans"
+	echo -e "\nChoose ${cyc}option${noc} from the ${grc}list${noc} above:"; read -r -e "installer_ans"
 else installer_ans="1"; fi
 if [[ "$installer_ans" == "1" ]]; then
 	sudo cp "$Exec_folder/histutilities" "$Bin_folder/histutilities"
-	cp -r "$Exec_folder/Configs" "$Hist_folder/"
+	cp -r "$Exec_folder/Installers" "$Hist_folder/"
 	sudo chmod 755 "$Bin_folder/histutilities"
 	if [[ -f "$Bin_folder/histutilities" ]]; then echo -e "\n${grc}Successful${noc}\n"
 	else echo -e "\n${rec}Failed${noc}\n"; fi
